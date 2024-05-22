551b0cb2a286cabab2579a529276e383528773be
yashbaraiya18 <gameryashraj098@gmail.com>
2024-05-22 10:49:38 +0530

    Delete src directory

--- src/State/Actions/index.js
+++
@@ -1,2 +0,0 @@
-export * as product_Action from './product.action'
-export * as number_Action from './number.action'
--- src/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import reducers from "./State/Reducers"
-
-const store = configureStore({
-    reducer: reducers
-})
-
-export default store
